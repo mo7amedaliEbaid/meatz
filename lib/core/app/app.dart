39b0c657c7ea6100fdafcaa6ae63874ff3c9d7dd
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-06-29 23:12:07 +0300

    refactor

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meatz/application/application.dart';
+import 'package:meatz/data/data_sources/about.dart';
+import 'package:meatz/data/repositories/about.dart';
+import 'package:meatz/domain/use_case/about.dart';
 import '../core.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,17 +12,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: AppRouter.splash,
-      theme: ThemeData(
-          fontFamily: AppStrings.fontFamily,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: AppColors.deepRed,
-            centerTitle: true,
-          )),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => AboutBloc(
+              getAbout: GetAbout(
+                aboutRepository: AboutRepositoryImpl(
+                  aboutRemoteDataSource: AboutRemoteDataSourceImpl(
+                    firestore: FirebaseFirestore.instance,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          initialRoute: AppRouter.splash,
+          theme: ThemeData(
+            fontFamily: AppStrings.fontFamily,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: AppColors.deepRed,
+              centerTitle: true,
+            ),
+          ),
+        ));
   }
 }
--- lib/presentation/screens/about.dart
+++ lib/presentation/screens/about.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meatz/application/application.dart';
+
+import '../../data/models/info.dart';
+import '../../domain/entities/info.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
@@ -9,7 +14,52 @@ class AboutScreen extends StatefulWidget {
 
 class _AboutScreenState extends State<AboutScreen> {
   @override
+  void initState() {
+    context.read<AboutBloc>().add(GetAboutEvent());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocBuilder<AboutBloc, AboutState>(builder: (context, state) {
+      if (state is AboutLoading) {
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      } else if (state is AboutError) {
+        const Scaffold(
+          body: Center(
+            child: Text("Error"),
+          ),
+        );
+      } else if (state is AboutLoaded) {
+        return AboutSuccessScreen(
+          about: state.about,
+        );
+      }
+      return const SizedBox.shrink();
+    });
+  }
+}
+
+class AboutSuccessScreen extends StatelessWidget {
+  const AboutSuccessScreen({super.key, required this.about});
+
+  final Info about;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
